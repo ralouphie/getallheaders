c0843800fda1965fcdbd6b2a3e5902edaef7754f
ralouphie <ralph.khattar@gmail.com>
2014-06-02 17:33:20 -0400

    PSR-2

--- tests/GetAllHeadersTest.php
+++ tests/GetAllHeadersTest.php
@@ -2,48 +2,48 @@
 
 class HttpBuildUrlTest extends \PHPUnit_Framework_TestCase
 {
-	public function testNormalCase()
-	{
-		$expected = array(
-			'Key-One'                 => 'foo',
-			'Key-Two'                 => 'bar',
-			'Another-Key-For-Testing' => 'baz'
-		);
+    public function testNormalCase()
+    {
+        $expected = array(
+            'Key-One'                 => 'foo',
+            'Key-Two'                 => 'bar',
+            'Another-Key-For-Testing' => 'baz'
+        );
 
-		$_SERVER['HTTP_KEY_ONE']                 = 'foo';
-		$_SERVER['HTTP_KEY_TWO']                 = 'bar';
-		$_SERVER['HTTP_ANOTHER_KEY_FOR_TESTING'] = 'baz';
+        $_SERVER['HTTP_KEY_ONE']                 = 'foo';
+        $_SERVER['HTTP_KEY_TWO']                 = 'bar';
+        $_SERVER['HTTP_ANOTHER_KEY_FOR_TESTING'] = 'baz';
 
-		$result = getallheaders();
+        $result = getallheaders();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
-	public function testContentType()
-	{
-		$expected = array(
-			'Content-Type' => 'two'
-		);
+    public function testContentType()
+    {
+        $expected = array(
+            'Content-Type' => 'two'
+        );
 
-		$_SERVER['HTTP_CONTENT_TYPE'] = 'one';
-		$_SERVER['CONTENT_TYPE']      = 'two';
+        $_SERVER['HTTP_CONTENT_TYPE'] = 'one';
+        $_SERVER['CONTENT_TYPE']      = 'two';
 
-		$result = getallheaders();
+        $result = getallheaders();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 
-	public function testContentLength()
-	{
-		$expected = array(
-			'Content-Length' => '222'
-		);
+    public function testContentLength()
+    {
+        $expected = array(
+            'Content-Length' => '222'
+        );
 
-		$_SERVER['HTTP_CONTENT_LENGTH'] = '111';
-		$_SERVER['CONTENT_LENGTH']      = '222';
+        $_SERVER['HTTP_CONTENT_LENGTH'] = '111';
+        $_SERVER['CONTENT_LENGTH']      = '222';
 
-		$result = getallheaders();
+        $result = getallheaders();
 
-		$this->assertEquals($expected, $result);
-	}
+        $this->assertEquals($expected, $result);
+    }
 }
